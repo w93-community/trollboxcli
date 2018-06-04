b8cbe66ea4d5dd76e231730e3e9d26f826b12fdc
Robbie Langer <robbie0630dev@gmail.com>
2018-06-04 13:31:34 -0400

    no more sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,13 @@ var gulp = require('gulp')
 var source = require('vinyl-source-stream')
 var buffer = require('vinyl-buffer')
 var browserify = require('browserify')
-var sourcemaps = require('gulp-sourcemaps')
-var log = require('gulplog')
 var buble = require('gulp-buble')
-var env = require('gulp-env')
 
 gulp.task('build', function() {
-  return browserify({
-    entries: './src/main.js',
-    debug: true
-  })
+  return browserify('./src/main.js')
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(buble())
-      .on('error', log.error)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./build/'))
+    .pipe(buble())
     .pipe(gulp.dest('./build/'))
 })
