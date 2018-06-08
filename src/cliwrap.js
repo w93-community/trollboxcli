aea5e4a4916c75ded9b2cc0dadf09cdbcb9b98e3
Robbie Langer <robbie0630dev@gmail.com>
2018-06-07 20:47:28 -0400

    removed async await from cli wrap

--- src/cliwrap.js
+++ src/cliwrap.js
@@ -1,30 +1,29 @@
-module.exports = async function(f) {
+module.exports = function(f) {
     var originalPrompt = this.cli.prompt.innerHTML
     var originalOnenter = this.cli.onenter
     var originalOndestroy = this.cli.ondestroy
-    try  {
-        this.cli.prompt.innerHTML = ''
-        this.cli.onenter = l => false
-        var cli = this.cli
-        var lastLog = $log('')
-        await f({
-            log: (...args) => {
-                var newLog = $log(...args)
-                lastLog.parentElement.insertBefore(newLog, lastLog.nextSibling)
-                lastLog = newLog
-            },
-            set online(f) {
-                cli.onenter = l => { f(l); return false }
-            },
-            set onexit(f) {
-                cli.ondestroy = () => f()
-            },
-            set prompt(p) { cli.prompt.innerHTML = p },
-            arg: this.arg
-        })
-    } finally {
+    this.cli.prompt.innerHTML = ''
+    this.cli.onenter = l => false
+    var cli = this.cli
+    var lastLog = $log('')
+    const cleanup = () => {
         this.cli.prompt.innerHTML = originalPrompt
         this.cli.onenter = originalOnenter
         this.cli.ondestroy = originalOndestroy
     }
+    return f({
+        log: (...args) => {
+            var newLog = $log(...args)
+            lastLog.parentElement.insertBefore(newLog, lastLog.nextSibling)
+            lastLog = newLog
+        },
+        set online(f) {
+            cli.onenter = l => { f(l); return false }
+        },
+        set onexit(f) {
+            cli.ondestroy = () => f()
+        },
+        set prompt(p) { cli.prompt.innerHTML = p },
+        arg: this.arg
+    }).then(cleanup).error(cleanup)
 }
